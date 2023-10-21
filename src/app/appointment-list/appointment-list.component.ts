02dcc328fa05bf4af92c81586fb213c1632b2d65
sadatmalik <sadat.malik@me.com>
2023-10-21 11:45:37 +0100

    add 2-way data binding for appointment title and date

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -8,10 +8,9 @@ import { Appointment } from "../models/appointment";
 })
 export class AppointmentListComponent {
 
-  appointment: Appointment = {
-    id: 1,
-    title: "Take dog for a walk",
-    date: new Date('2023-10-21')
-  };
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+
+  appointments: Appointment[] = [];
 
 }
