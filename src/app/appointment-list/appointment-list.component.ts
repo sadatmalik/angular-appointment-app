44850da52581c0f09a433dff4f80e637b4ed87cc
sadatmalik <sadat.malik@me.com>
2023-10-21 18:45:30 +0100

    add delete appointment method and set it as a click listener

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -26,4 +26,8 @@ export class AppointmentListComponent {
       this.newAppointmentDate = new Date();
     }
   }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+  }
 }
